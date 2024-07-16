c7906e29839ea4eed831cb781999e9492555f710
DamianPodrazaCode <damian.podraza@gmail.com>
2024-07-16 22:47:10 +0200

    Update 01_GPIO.ino

--- 01_GPIO/01_GPIO.ino
+++ 01_GPIO/01_GPIO.ino
@@ -1,3 +1,43 @@
+/*
+Nie ze wszystkich pinów można kożystać jako zwykłe IN-OUT
+
+PIN - OPIS (bez opisu zwykły IO)
+0	  - Default-PullUp, boot-PWM
+1	  - Boot-DEBUG
+2	  - Default-PullDown
+3	  - Boot-HIGH
+4	
+5	  - Default-PullUp, boot fail if pulled down, boot-PWM
+6	  - Wewnętrznie SPI FLASH i PSRAM
+7	  - Wewnętrznie SPI FLASH i PSRAM
+8	  - Wewnętrznie SPI FLASH i PSRAM
+9	  - Wewnętrznie SPI FLASH i PSRAM
+10	- Wewnętrznie SPI FLASH i PSRAM
+11	- Wewnętrznie SPI FLASH i PSRAM
+12	- Default-PullDown, boot fail if pulled high
+13	
+14	- Outputs PWM signal at boot
+15	- Default-PullUp, boot fail if pulled down,  Outputs PWM signal at boot
+16	
+17	
+18	
+19	
+21	
+22	
+23	
+25	
+26	
+27	
+32	
+33	
+34	- Tylko INPUT (bez PULLUP i DOWN)
+35	- Tylko INPUT (bez PULLUP i DOWN)
+36	- Tylko INPUT (bez PULLUP i DOWN)
+37	- Podpięte w module przez kondensator do 36
+38	- Podpięte w module przez kondensator do 39
+39	- Tylko INPUT (bez PULLUP i DOWN)
+*/
+
 void setup() {
 
   pinMode(16, OUTPUT);
